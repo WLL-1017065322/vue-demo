49a3afc9fc1fe09c15350eecab4d7f9fc44b2d48
WLL-1017065322 <1017065322@qq.com>
2019-05-07 20:32:58 +0800

    swiper001

--- 去哪儿/travel/src/views/Home.vue
+++ 去哪儿/travel/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <home-header>header</home-header>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
@@ -8,11 +9,13 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
 import HomeHeader from '@/components/Header.vue';
+import HomeSwiper from '@/components/Swiper.vue';
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
+    HomeSwiper,
   },
 };
 </script>
