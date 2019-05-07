4ad8b48a27a8a27e09dab083382ce1170adda7a0
WLL-1017065322 <1017065322@qq.com>
2019-05-07 22:15:21 +0800

    icons57

--- 去哪儿/travel/src/views/Home.vue
+++ 去哪儿/travel/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 // import HelloWorld from '@/components/HelloWorld.vue';
 import HomeHeader from '@/components/Header.vue';
 import HomeSwiper from '@/components/Swiper.vue';
+import HomeIcons from '@/components/Icons.vue';
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
+    HomeIcons,
   },
 };
 </script>
