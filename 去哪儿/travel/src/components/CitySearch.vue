4460fddb40e45f6b69ef64f8a3615db9db17f9bb
WLL-1017065322 <1017065322@qq.com>
2019-05-09 17:08:40 +0800

    city-search-logic 5917

--- 去哪儿/travel/src/components/CitySearch.vue
+++ 去哪儿/travel/src/components/CitySearch.vue
@@ -1,20 +1,60 @@
 <template>
   <div>
     <div class="search">
-      <input type="text" placeholder="输入城市名或者拼音" class="search-input">
+      <input type="text" placeholder="输入城市名或者拼音" class="search-input" v-model="keyword">
     </div>
-    <!-- <div class="search-content">
+    <div class="search-content"  v-show="keyword" ref="search">
       <ul>
-        <li></li>
+        <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
       </ul>
-    </div> -->
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "CitySearch",
+  props: {
+    cities: Object
+  },
   data() {
-    return {};
+    return {
+      keyword: "",
+      list: [],
+      timer: null
+    };
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      if (!this.keyword) {
+        this.list = [];
+        return;
+      }
+      this.timer = setTimeout(() => {
+        const result = [];
+        for (let i in this.cities) {
+          this.cities[i].forEach(value => {
+            if (
+              value.spell.indexOf(this.keyword) > -1 ||
+              value.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(value);
+            }
+          });
+        }
+        this.list = result;
+      }, 100);
+    }
   }
 };
 </script>
@@ -34,24 +74,23 @@ export default {
     border-radius: 0.06rem;
     color: #666;
   }
- 
 }
 
- .search-content {
-    // height: 1rem;  
-    z-index: 1;
-    overflow: hidden;
-    position: absolute;
-    top: 1.58rem;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: #eee;
-    .search-item {
-      line-height: 0.62rem;
-      padding-left: 0.2rem;
-      background: #fff;
-      color: #666;
-    }
+.search-content {
+  // height: 1rem;
+  z-index: 1;
+  overflow: hidden;
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #eee;
+  .search-item {
+    line-height: 0.62rem;
+    padding-left: 0.2rem;
+    background: #fff;
+    color: #666;
   }
+}
 </style>
