a1255489f73c6afff8c4baeb35b599cdcf0865c0
WLL-1017065322 <1017065322@qq.com>
2019-05-09 22:04:29 +0800

    city-keepalive5922

--- 去哪儿/travel/src/views/Home.vue
+++ 去哪儿/travel/src/views/Home.vue
@@ -17,6 +17,7 @@ import HomeIcons from '@/components/Icons.vue';
 import HomeRecommend from '@/components/Recommend.vue';
 import HomeWeekend from '@/components/Weekend.vue';
 import axios from 'axios';
+import { mapState } from 'vuex'
 
 
 export default {
@@ -38,10 +39,13 @@ export default {
 
     };
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo() {
       // axios.get('/api/index.json')
-      axios.get('api/index.json')
+      axios.get('api/index.json?city='+this.city)
         .then(this.getHomeInfoSucc);
     },
     getHomeInfoSucc(res) {
@@ -61,5 +65,11 @@ export default {
   mounted() {
     this.getHomeInfo();
   },
+  activated () {
+    if(this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
+  }
 };
 </script>
